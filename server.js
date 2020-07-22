2aecb1e82983aabbec77012a76d1b1f1e8911207
Sebastian <sanabal@alumnos.ubiobio.cl>
2020-07-22 16:28:29 -0400

    segundo commit

--- server.js
+++ server.js
@@ -1,38 +1,33 @@
 const express = require('express');
 const app = express();
 
-var hbs = require('hbs');
-
+const hbs = require('hbs');
 require('./hbs/helpers');
 
 const port = process.env.PORT || 3000;
 
 
+app.use(express.static(__dirname + '/public'));
 
-app.use(express.static(__dirname + '/public'))
-
-//Express HBS engine
-//hbs.registerPartials(__dirname + 'views/parciales')
-hbs.registerPartials(__dirname + '/views/parciales', function(err) {});
-
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
 
 
 
 
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
 
     res.render('home', {
-        nombre: 'sebastian anabalon',
+        nombre: 'sebastian'
     });
-})
+});
 
-app.get('/about', function(req, res) {
+app.get('/about', (req, res) => {
 
-    res.render('about', {});
-})
+    res.render('about');
+});
 
-
-app.listen(8080, () => {
-    console.log(`Escuchando peticiones en el puerto ${port}`)
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
 });
